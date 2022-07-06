c51199bd4ed01bc246f7ec63307f07fa18afdee4
maxgavaga <ferrarif138@yandex.ru>
2022-07-06 13:36:54 +0300

    Task-1

---
+++ Task 1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте двумерный массим m*x, заполненный случайными вещественными числами.
+
+Console.WriteLine("Введите количество строк (m)");
+int linesVol = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов (x)");
+int columnsVol = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Получившийся двумерный массив:");
+double[,] numbers = new double[linesVol, columnsVol];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-50, 50)) / 10;
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " |");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
